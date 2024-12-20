219267e0b39a712495b23c7e8f3223ed69935f91
Sunil0881 <sunil12162003@gmail.com>
2024-12-20 10:04:34 +0530

    backend url added

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,4 @@
-export const BACKEND_URL="http://13.51.195.135:5000"
+//export const BACKEND_URL="http://13.51.195.135:5000"
+export const BACKEND_URL="https://trsabackend.vercel.app"
 
 //url added
