b4d2dcbb005c9d1036385a75acaba25839183935
gokkull-15 <gokkull04@gmail.com>
2025-02-14 17:18:21 +0530

    constants

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,8 @@
 //export const BACKEND_URL="http://13.51.195.135:5000"
 
 
-// export const BACKEND_URL="https://trsabackend.vercel.app"
-export const BACKEND_URL = "http://localhost:5000"
+export const BACKEND_URL="https://trsabackend.vercel.app"
+// export const BACKEND_URL = "http://localhost:5000"
 
 
 // PORT=443 && react-scripts start
